77d1bef28023facd08767167a9dad3ecc77a89f8
lucas-duchene-vinci <lucas.duchene@student.vinci.be>
2025-10-05 18:12:55 +0200

    new 1.6

--- exercices/1.6/myMovies/routes/films.ts
+++ exercices/1.6/myMovies/routes/films.ts
@@ -98,5 +98,83 @@ router.post("/", (req, res) => {
     return res.send(200).json(newFilm);
 });
 
+router.delete("/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const index = defaultFilms.findIndex((film) => film.id === id);
 
+    if(index === -1) {
+        return res.status(404).send("Film not found");
+    }
+
+    const deletedElements = defaultFilms.splice(index, 1);
+    return res.status(200).json(deletedElements[0]);
+});
+
+router.patch("/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const film = defaultFilms.find((film) => film.id === id);
+
+    if(!film) {
+        return res.status(404).send("Film not found");
+    }
+
+    const body: unknown = req.body
+
+    if (
+        !body || 
+        typeof body !== "object" ||
+        ("title" in body && typeof body.title !== "string") ||
+        ("director" in body && typeof body.director !== "string") ||
+        ("duration" in body && typeof body.duration !== "number")
+    ) {
+        return res.status(400).send("Parameter invalid");
+    }
+
+    const { title, director, duration }: Partial<NewFilms> = body;
+
+    if(title) {
+        film.title = title;
+    }
+
+    if(director) {
+        film.director = director;
+    }
+
+    if(duration) {
+        film.duration = duration;
+    }
+
+    return res.status(200).json(film);
+});
+
+router.put("/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const film = defaultFilms.find((film) => film.id === id);
+
+    if(!film) {
+        return res.status(404).send("Film not found");
+    }
+
+    const body: unknown = req.body;
+
+    if (
+        !body || 
+        typeof body !== "object" ||
+        !("title" in body) ||
+        !("director" in body) ||
+        !("duration" in body) ||
+        typeof body.title !== "string" ||
+        typeof body.director !== "string" ||
+        typeof body.duration !== "number" ||
+        !body.title.trim() ||
+        !body.director.trim() ||
+        body.duration <= 0
+    ) {
+        return res.status(400).send("Parameter invalid");
+    }
+
+    const { title, director, duration } = body as NewFilms;
+
+    return res.status(200).json(defaultFilms.push({id, title, director, duration}));
+});
 export default router
