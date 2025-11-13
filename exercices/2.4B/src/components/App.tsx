71adbd664bb5a384961655da789831a1eb01e406
lucas-duchene-vinci <lucas.duchene@student.vinci.be>
2025-11-13 16:17:28 +0100

    new: ex 2.4B

---
+++ exercices/2.4B/src/components/App.tsx
@@ -0,0 +1,19 @@
+import type { User } from "../types";
+import UserCard from "./UserCard";
+
+const users: User[] = [
+  { name: "John Doe", age: 25, isOnline: true },
+  { name: "Jane Doe", age: 22, isOnline: false },
+  { name: "Foo Bar", age: 30, isOnline: true },
+];
+
+const App = () => (
+  <div>
+    <h1>Users</h1>
+    {users.map((user, index) => (
+      <UserCard key={index} user={user} />
+    ))}
+  </div>
+);
+
+export default App;
